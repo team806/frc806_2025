aaa9b49dfc2574e9f3a73b1dbe4771f6f3167aa6
Jeff Stubler <brunsa2@gmail.com>
2025-02-22 13:56:32 -0500

    Add controller and prep for bring-up

--- src/main/java/frc/robot/Subsystems/DrivetrainSubsystem.java
+++ src/main/java/frc/robot/Subsystems/DrivetrainSubsystem.java
@@ -3,6 +3,8 @@ package frc.robot.Subsystems;
 
 import com.ctre.phoenix6.hardware.Pigeon2;
 
+import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.filter.SlewRateLimiter;
 
 //import com.ctre.phoenix6.hardware.Pigeon2;
@@ -19,6 +21,9 @@ import edu.wpi.first.wpilibj.ADIS16470_IMU;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
+
+import static edu.wpi.first.wpilibj2.command.Commands.parallel;
+import static edu.wpi.first.wpilibj2.command.Commands.waitUntil;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
@@ -41,12 +46,28 @@ public class DrivetrainSubsystem extends SubsystemBase{
         SlewRateLimiter rotationLimiter = new SlewRateLimiter(rotationMaxAccelerationRadiansPerSecondSquared);
         PhotonCamera camera = new PhotonCamera("photonvision");
 
+        private final PIDController visionForwardBackController = new PIDController(0, 0, 0);
+        private final PIDController visionSidewaysController = new PIDController(0, 0, 0);
+        private final PIDController visionRotationsController = new PIDController(0, 0, 0);
+
     //CONSTRUCTOR//
         public DrivetrainSubsystem(swerveModule... modules) {
             IMU = new ADIS16470_IMU();
             //pigeon = new Pigeon2(Constants.PigeonID,"Default Name");
             this.modules = modules;
             kinematics = new SwerveDriveKinematics(Constants.moduleLocations);
+
+            visionForwardBackController.setTolerance(0);
+            visionSidewaysController.setTolerance(0);
+            visionRotationsController.setTolerance(0);
+
+            setDefaultCommand(
+                runOnce(
+                        () -> {
+                        drive(new ChassisSpeeds(0, 0, 0));
+                        })
+                    .andThen(run(() -> {}))
+                    .withName("Idle"));
 
         }
     //SINGLETON//
@@ -112,13 +133,37 @@ public class DrivetrainSubsystem extends SubsystemBase{
         }
 
         public Command executeAimCommand() {
-            return runOnce(() -> {
-                var result = camera.getLatestResult();
-                boolean hasTargets = result.hasTargets();
-                PhotonTrackedTarget target = result.getBestTarget();
-                var translation = target.getBestCameraToTarget()
-                var xDistanceToTravel = translation.getX() - Constants.Motion.xoffset;
-                
+            return parallel(
+                run(() -> {
+                    // TODO: need to capture on the first iteration, but then continue if interrupted for a certain amount of time before aborting
+                    var results = camera.getAllUnreadResults();
+                    if (!results.isEmpty()) {
+                        var result = results.get(results.size() - 1);
+                        if (result.hasTargets()) {
+                            var bestTarget = result.getBestTarget();
+                            if (bestTarget != null) {
+                                var translation = bestTarget.getBestCameraToTarget();
+                                var forwardVelocity = MathUtil.clamp(visionForwardBackController.calculate(translation.getX(), 0), -0.1, 0.1);
+                                var sidewaysVelocity = MathUtil.clamp(visionSidewaysController.calculate(translation.getY(), 0), -0.1, 0.1);
+                                var angularVelcoity = MathUtil.clamp(visionRotationsController.calculate(bestTarget.getYaw(), 0), -0.1, 0.1);
+
+                                var speeds = new ChassisSpeeds(forwardVelocity, sidewaysVelocity, angularVelcoity);
+                                drive(speeds);
+                            }
+                        }
+                    }
+                }),
+                waitUntil(() ->
+                    visionForwardBackController.atSetpoint() &&
+                        visionSidewaysController.atSetpoint() &&
+                        visionRotationsController.atSetpoint()
+                )
+            );
+        }
+
+        public Command executeDriveForwardCommand() {
+            return run(() -> {
+                driveFieldRelative(new ChassisSpeeds(0.1, 0, 0));
             });
         }
     ////
